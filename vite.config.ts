33d1e56cdd444e21d3de416d778898e443242dff
zheton90 <jen.nikolenko@yandex.ru>
2026-01-19 18:37:27 +0300

    before 2 variant rtkquery

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todolistProject/",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
