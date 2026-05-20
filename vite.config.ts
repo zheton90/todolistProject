2d8e9393d738501ea9529b3332a52c649350e48e
zheton90 <jen.nikolenko@yandex.ru>
2026-05-20 23:49:21 +0300

    make a style2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/todolistProject/",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
